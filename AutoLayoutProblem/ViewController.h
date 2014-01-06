7f1e5bdb927f364dba4e3fddf3263fe0ab65d14c
Arnold Schmucker <arnieschmucker@gmail.com>
2014-01-05 16:28:52 -0800

    First Commit.

--- AutoLayoutProblem/ViewController.h
+++ AutoLayoutProblem/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *topLeftView;
+@property (weak, nonatomic) IBOutlet UIView *topRightView;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
 
 @end
